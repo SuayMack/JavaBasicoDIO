243eb5cb794278cdb7ff9e08edeef6fa61df72e1
SuayMack <pry_chu@hotmail.com>
2023-01-08 03:19:50 -0300

    create SmartTv

---
+++ smartTv/src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+  public static void main(String[] args) throws Exception{
+    SmartTv smartTv = new SmartTv();
+  
+
+    smartTv.ligar();
+    System.out.println("TV ligada? " + smartTv.ligada);
+    System.out.println("Volume atual: " + smartTv.volume);
+    System.out.println("Canal atual: " + smartTv.canal);
+    smartTv.mudarCanal(13);
+    System.out.println("Novo status ->canal? " + smartTv.canal);
+    smartTv.aumentarCanal();
+    System.out.println("Novo canal ->canal " + smartTv.canal);
+    smartTv.diminuirCanal();
+    System.out.println("Novo canal ->canal " + smartTv.canal);
+  }
+  
+}
