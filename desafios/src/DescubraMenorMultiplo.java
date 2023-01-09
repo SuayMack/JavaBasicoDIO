37a14895e7e25a9f355b521098447235ab009fc6
SuayMack <pry_chu@hotmail.com>
2023-01-09 13:32:36 -0300

    Update challenges

---
+++ desafios/src/DescubraMenorMultiplo.java
@@ -0,0 +1,25 @@
+public class DescubraMenorMultiplo {
+  /*// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner,
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);".
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int numero = scanner.nextInt();
+
+//TODO: Crie as outras condições necessárias para a resolução do desafio.
+    int minimoMultiplo = 0;
+    if (numero % 2 == 0) {
+      minimoMultiplo = numero;
+    } else {
+      minimoMultiplo = numero * 2;
+    }
+
+    System.out.println("O resultado eh:" + minimoMultiplo);
+  }
+
+} */
+}
---
+++ desafios/src/EncontrandoPercentualDesconto.java
@@ -0,0 +1,21 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.  
+/* import java.util.*;
+ 
+public class Main{
+
+  public static void main(String args[]) {
+      Scanner input = new Scanner(System.in);
+  		
+      int M = input.nextInt();
+      int S = input.nextInt();  
+      
+      //TODO:  Retorne o percentual de desconto que foi aplicado no produto
+      int porcentagem = ((S*100)/M);
+      int desconto = 100 - porcentagem;
+      System.out.println("O desconto foi de " + desconto + "%");
+     
+    
+  }
+} */
